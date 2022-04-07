8051aba1da774a5a19fe7f2516cc34947a1d8687
Zahraah <2136717@students.wits.ac.za>
2022-04-07 21:06:51 +0200

    login functionality complete using local db

--- login.js
+++ login.js
@@ -4,6 +4,9 @@ const express = require("express");
 const app = express();
 app.use("/assets", express.static("assets"));
 
+const bodyParser = require("body-parser");
+const encoder = bodyParser.urlencoded();
+
 const connection = mysql.createConnection({
     host: "localhost",
     user: "root",
@@ -22,5 +25,24 @@ app.get("/", function(req,res){
 })
 
 
+app.post("/", encoder, function(req, res){
+    var username = req.body.username;
+    var password = req.body.password;
+
+    connection.query("select * from loginuser where user_name = ? and user_pass = ?", [username,password], function(error, results, fields){
+        if(results.length > 0){
+            res.redirect("/index");
+        }else{
+            res.redirect("/");
+        }
+        res.end();
+    })
+})
+
+//login is successful
+app.get("/index", function(req,res){
+    res.sendFile(__dirname + "/index.html")
+})
+
 //set app port
-app.listen(4000);
+app.listen(4900);
